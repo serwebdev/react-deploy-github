f9974dd74559a5faef44f6e32c67ce42e94ea68f
Sergey <szhernakov30@gmail.com>
2024-08-12 14:23:36 +0500

    change error page

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -2,11 +2,13 @@ import { createBrowserRouter } from 'react-router-dom';
 import RootLayout from '../layouts/RootLayout';
 import Home from '../pages/Home';
 import About from '../pages/About';
+import ErrorPage from '../pages/ErrorPage';
 
 export const router = createBrowserRouter([
   {
     path: '/',
     element: <RootLayout />,
+    errorElement: <ErrorPage />,
     children: [
       {
         index: true,
