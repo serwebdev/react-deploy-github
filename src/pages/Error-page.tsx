eb08b37a4fecd4cff10339b3faf509269a8ed798
Sergey <szhernakov30@gmail.com>
2024-08-12 14:16:57 +0500

    add error page

---
+++ src/pages/Error-page.tsx
@@ -0,0 +1,25 @@
+import { useLocation, useRouteError } from 'react-router-dom';
+
+interface IError {
+  statusText: string;
+  message: string;
+}
+
+export default function ErrorPage() {
+  const error = useRouteError() as IError;
+
+  const location = useLocation();
+
+  console.error(error);
+  console.log(location);
+
+  return (
+    <div id="error-page">
+      <h1>Oops!</h1>
+      <p>Sorry, an unexpected error has occurred.</p>
+      <p>
+        <i>{error.statusText || error.message}</i>
+      </p>
+    </div>
+  );
+}
