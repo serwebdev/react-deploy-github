4ecf807e58fc7a7f28e3a17c4d5431f316f03ba2
Sergey <szhernakov30@gmail.com>
2025-02-02 12:41:22 +0500

    fix router

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -1,4 +1,4 @@
-import { createHashRouter } from 'react-router-dom';
+import { createBrowserRouter } from 'react-router-dom';
 import RootLayout from '../layouts/RootLayout';
 import Home from '../pages/Home';
 import About from '../pages/About';
@@ -6,8 +6,7 @@ import ErrorPage from '../pages/ErrorPage';
 
 const BASE_URL = import.meta.env.BASE_URL;
 
-// export const router = createBrowserRouter([
-export const router = createHashRouter([
+export const router = createBrowserRouter([
   {
     path: BASE_URL,
     element: <RootLayout />,
