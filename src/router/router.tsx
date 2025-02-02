bc5667134bc0366fc23d9c81c81d2a724c3c467c
Sergey <szhernakov30@gmail.com>
2025-02-02 12:13:04 +0500

    replace createBrowserRouter to createHashRouter

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -1,4 +1,4 @@
-import { createBrowserRouter } from 'react-router-dom';
+import { createBrowserRouter, createHashRouter } from 'react-router-dom';
 import RootLayout from '../layouts/RootLayout';
 import Home from '../pages/Home';
 import About from '../pages/About';
@@ -6,7 +6,8 @@ import ErrorPage from '../pages/ErrorPage';
 
 const BASE_URL = import.meta.env.BASE_URL;
 
-export const router = createBrowserRouter([
+// export const router = createBrowserRouter([
+export const router = createHashRouter([
   {
     path: BASE_URL,
     element: <RootLayout />,
